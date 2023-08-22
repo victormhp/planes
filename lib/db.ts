ea5b0c25ee77c74d58413a1bc0dd1cdcb3e4c096
victormhp <victoralbertomhp@gmail.com>
2023-08-22 00:15:43 -0600

    Update db env

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,7 @@
 import postgres from "postgres";
 
-const env = process.env.NODE_ENV === 'production' ? process.env.SUPABASE_URL ?? '' : process.env.DATABASE_URL ?? ''
-const connectionString = env 
+const databaseEnv = process.env.NODE_ENV === 'production' ? process.env.SUPABASE_URL : process.env.DATABASE_URL
+const connectionString = databaseEnv ?? ''
 const sql = postgres(connectionString)
 
 export default sql
